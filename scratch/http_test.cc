115e55b2d3dad426c9ed55486836b347c49dcdb8
liyilun <530625549@qq.com>
2020-11-26 18:00:02 +0800

    cnt

--- scratch/http_test.cc
+++ scratch/http_test.cc
@@ -72,7 +72,7 @@ ClientMainObjectReceived (Ptr<const ThreeGppHttpClient> client, Ptr<const Packet
     {
       NS_LOG_INFO ("Client has successfully received a main object of "
                    << p->GetSize () << " bytes."<<" deadline is :"<<client->GetDeadline());
-      if(Simulator::Now()>client->GetDeadline()){
+      if(Simulator::Now()<=client->GetDeadline()){
         not_miss_count++;
       }
       all_cnt++;
